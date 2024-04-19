75813d9af095557853df8dc2d9c4d0bd526b0d83
MavlonbekKhakijanov <khakijanovamor@gmail.com>
2024-04-19 15:03:06 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let firstName = prompt("ismingizni kiriting");
+let surName = prompt("familyangizni kiriting");
+const educationCenter = "Najot ta'lim";
+alert(
+  `Salom ${firstName.charAt().toUpperCase()}${firstName
+    .slice(1)
+    .toLowerCase()}  ${surName.charAt().toUpperCase()}${surName
+    .slice(1)
+    .toLowerCase()} ${educationCenter}ga hush kelibsz`
+);
